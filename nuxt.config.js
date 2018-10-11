1f4379e854d2d5e5c7c79addcdc228a6aa1e8d00
byrnecode <byrne.code@gmail.com>
2018-10-11 11:23:53 -0400

    Vuesax installation

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,6 +33,8 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '~/plugins/material-icons',
+    '~/plugins/vuesax'
   ],
 
   /*
--- pages/index.vue
+++ pages/index.vue
@@ -18,6 +18,25 @@
           target="_blank"
           class="button--grey">GitHub</a>
       </div>
+
+      <div>
+        <vs-button color="primary" vs-type="filled">Primary</vs-button>
+        <vs-button color="success" vs-type="filled">Success</vs-button>
+        <vs-button color="danger" vs-type="filled">Danger</vs-button>
+        <vs-button color="warning" vs-type="filled">Warning</vs-button>
+        <vs-button color="dark" vs-type="filled">Dark</vs-button>
+        <vs-button color="rgb(134, 4, 98)" vs-type="filled">RGB</vs-button>
+        <vs-button disabled vs-type="filled">Disabled</vs-button>
+        <vs-button vs-color="primary" vs-type="filled" to="/components/list.html">Router</vs-button>
+      </div>
+      <div>
+        <vs-button color="primary" vs-type="border" vs-icon="home">Home</vs-button>
+        <vs-button color="warning" vs-type="filled" vs-icon="star">Star</vs-button>
+        <vs-button color="success" vs-type="flat" vs-icon="done">Done</vs-button>
+        <vs-button color="dark" vs-type="line" vs-icon="menu">menu</vs-button>
+        <vs-button color="danger" vs-type="gradient" vs-icon="favorite">favorite</vs-button>
+        <vs-button disabled color="primary" vs-type="border" vs-icon="home">Disabled</vs-button>
+      </div>
     </div>
   </section>
 </template>
